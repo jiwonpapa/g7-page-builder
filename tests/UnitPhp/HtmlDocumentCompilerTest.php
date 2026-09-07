119b60a71c71d9305699cd00ab1eb176e6fbec43
Baek Jinseok <baegjins@gmail.com>
2026-09-07 15:05:38 +0900

    task(ep2-preset-count-contract-20260907): submit worktree changes

--- tests/UnitPhp/HtmlDocumentCompilerTest.php
+++ tests/UnitPhp/HtmlDocumentCompilerTest.php
@@ -1148,7 +1148,7 @@ final class HtmlDocumentCompilerTest extends TestCase
         self::assertIsString($contents);
         $manifest = json_decode($contents, true, flags: JSON_THROW_ON_ERROR);
         $quality = json_decode((string) file_get_contents(dirname(__DIR__, 2).'/resources/block-packs/builtin-core/product-quality.json'), true, flags: JSON_THROW_ON_ERROR);
-        self::assertCount($quality['rules']['inventory']['preset_count'], $manifest['presets']);
+        self::assertCount($quality['contract']['inventory']['preset_count'], $manifest['presets']);
 
         foreach (array_values($manifest['presets']) as $index => $preset) {
             $document = new PageBuilderDocument(

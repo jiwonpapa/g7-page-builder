8aa75064aeab072149becf217051d8667a3e51e1
Baek Jinseok <baegjins@gmail.com>
2026-09-08 15:50:56 +0900

    task(editor-asset-identity-20260908): submit worktree changes

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -1,11 +1,16 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', $locale) }}">
 <head>
+    @php
+        $editorDist = base_path('modules/jiwonpapa-page_builder/dist');
+        $editorCssDigest = hash_file('sha256', $editorDist.'/css/page-builder-editor.css');
+        $editorJsDigest = hash_file('sha256', $editorDist.'/js/page-builder-editor.iife.js');
+    @endphp
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex,nofollow">
     <title>G7 Page Builder</title>
-    <link rel="stylesheet" href="{{ url('/api/modules/assets/jiwonpapa-page_builder/dist/css/page-builder-editor.css') }}">
+    <link rel="stylesheet" href="{{ url('/api/modules/assets/jiwonpapa-page_builder/dist/css/page-builder-editor.css') }}?v={{ $editorCssDigest }}">
 </head>
 <body class="g7pb-editor-shell">
     <div hidden data-g7pb-runtime-config="{{ json_encode($siteRuntimeConfig ?? [], JSON_THROW_ON_ERROR) }}"></div>
@@ -23,6 +28,6 @@
             }
         })();
     </script>
-    <script defer src="{{ url('/api/modules/assets/jiwonpapa-page_builder/dist/js/page-builder-editor.iife.js') }}"></script>
+    <script defer src="{{ url('/api/modules/assets/jiwonpapa-page_builder/dist/js/page-builder-editor.iife.js') }}?v={{ $editorJsDigest }}"></script>
 </body>
 </html>
